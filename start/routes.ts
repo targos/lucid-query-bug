40f2dcd30e9abbe647045ee585c763702cc995a0
Michaël Zasso <targos@protonmail.com>
2021-10-13 11:33:45 +0200

    implement /posts route to show bug

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,17 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
+import Post from 'App/Models/Post'
 
 Route.get('/', async () => {
-  return 'Hello world from a slim app'
+  return 'Access /posts or /posts?user=Robert'
+})
+
+Route.get('/posts', async ({request}) => {
+  const query = Post.query().preload('createdBy')
+  const { user } = request.qs()
+  if (user) {
+    query.whereHas('createdBy', (userQuery) => {
+      userQuery.where('name', 'like', `%${user}%`)
+    })
+  }
+  return query
 })
